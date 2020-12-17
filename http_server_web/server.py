d2a771ff3e0a130582fe2b14d957463d85128aaf
voyagerdva <mariner-9@yandex.ru>
2020-12-17 22:36:52 +0300

    web server part 1

---
+++ http_server_web/server.py
@@ -0,0 +1,17 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class myHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()    
+
+        message = "Server is UP !!!"
+
+        self.wfile.write(bytes(message, "utf8"))
+        
+        return
+
+server = HTTPServer(("127.0.0.1", 8081), myHandler)
+server.serve_forever()
+
