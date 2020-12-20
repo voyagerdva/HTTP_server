4e95c7983833d9f26ddc4b489f6334c80d0ec5d0
voyagerdva <mariner-9@yandex.ru>
2020-12-20 19:31:49 +0300

    changed russian words to english

--- http_server_web/server.py
+++ http_server_web/server.py
@@ -2,15 +2,21 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()    
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        path = self.path
+        if path == "/":
+            path = "/index"
 
-        message = "Server is UP !!!"
+        try:
+            file  = open("pages"+path + ".html", 'r')
+        except FileNotFoundError:
+            file  = open("pages/404.html", 'r')
 
-        self.wfile.write(bytes(message, "utf8"))
-        
+        message = file.read()
+        file.close()
+        self.wfile.write(bytes(message, "utf-8"))
         return
 
-server = HTTPServer(("127.0.0.1", 8081), myHandler)
+server = HTTPServer(('127.0.0.1', 8081), myHandler)
 server.serve_forever()
-
